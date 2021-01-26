d35ad006b0577db49a4cda8d860d28dcc856839b
Fran2304 <lilatiravanttiarteaga@gmail.com>
2021-01-26 08:34:29 -0500

    images y html primera pantalla

---
+++ src/components/productsHome/ProductHome.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProductHome = (props) => {
+  const {image} = props
+  return(
+  <section>
+    <img src={image} alt=""/>
+    <p></p>
+  </section>
+)}
+
+export default ProductHome;
